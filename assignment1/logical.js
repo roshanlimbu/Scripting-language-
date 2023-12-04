f6900987df587484b27a0e7eb3e25b59a39878d3
Roshan Limbu <topazkayle@gmail.com>
2023-12-04 07:27:27 +0545

    logical operator example

---
+++ assignment1/logical.js
@@ -0,0 +1,10 @@
+let firstNum = 10;
+let secondNum = 10;
+if (firstNum === secondNum) {
+  console.log("Firstnum is equal to secondNum.");
+} else if (firstNum >= secondNum) {
+  console.log("First number is greater.");
+} else {
+  console.log("First number is smaller.");
+}
+
