978d38a774a8b010fbc3e63a4cebe7a043c68ca3
Roshan Limbu <topazkayle@gmail.com>
2023-12-04 07:33:53 +0545

    logical and or and not example done

---
+++ assignment1/operator3.js
@@ -0,0 +1,10 @@
+let x = 5;
+let y = 10;
+if (!(x == y)) {
+  console.log("logical NOT");
+} else if (x === 5 && y === 10) {
+  console.log("Logical AND");
+} else if (x === 5 || y === 10) {
+  console.log("Logical OR");
+}
+
