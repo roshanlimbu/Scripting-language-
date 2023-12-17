57a2330a7624aa56d71497ab8a2fd54da9109e4d
roshanlimbu <birenlimbu415@gmail.com>
2023-12-17 07:08:09 +0545

    conditional statement

---
+++ conditional.js
@@ -0,0 +1,8 @@
+let firstNum = 10;
+if (firstNum % 2 == 0) {
+  console.log("The number is even.");
+} else if (firstNum % 2 != 0) {
+  console.log("The number is odd.")
+} else {
+  console.log("The number is zero.");
+}
