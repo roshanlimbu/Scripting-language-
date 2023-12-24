e3136f69f5711852d5827678b03657a2a3e8b356
roshanlimbu <birenlimbu415@gmail.com>
2023-12-24 06:56:59 +0545

    -updated

--- conditional.js
+++ conditional.js
@@ -4,5 +4,5 @@ if (firstNum % 2 == 0) {
 } else if (firstNum % 2 != 0) {
   console.log("The number is odd.")
 } else {
-  console.log("The number is zero.");
-}
+  console.log("The number is zero.")}
+
