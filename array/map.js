9c616d1a2f95b7a39f307402e893d4dfd0810953
roshanlimbu <birenlimbu415@gmail.com>
2024-01-07 21:15:27 +0545

    updted

---
+++ array/map.js
@@ -0,0 +1,6 @@
+/* 
+ 13. Write a javascript program to create an array of objects representing students
+where each object has a name and age property. Use the map() method to
+create a new array containing only the ages of the students. Print the new array.
+*/
+const students = [{ name: "Roshan Limbu", age: 21 }, { name: "Ashish Limbu", age: 21 }, { name: "Kapil Tamang", age: 21 }]
