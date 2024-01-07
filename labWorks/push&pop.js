7a9aa671cd8741abc147e2ffc430d763ef0223c6
roshanlimbu <birenlimbu415@gmail.com>
2024-01-07 08:30:18 +0545

    -push & pop

---
+++ labWorks/push&pop.js
@@ -0,0 +1,19 @@
+/* Write a javascript program to create an array of objects representing people,
+where each object has a name and age property. Use push() to add a new
+person to the array, and use pop() to remove the last person from the array.*/
+
+const people = [{ id: 1, name: "Roshan limbu" }, { id: 2, name: "Bisnu limbu" }, { id: 3, name: "Kapil Tamang" }];
+people.push({ id: 4, name: "Rakesh Niraula" });
+people.push({ id: 5, name: "Ashish Khadka" });
+console.log(people);
+
+
+people.pop();
+
+
+console.log(people);
+
+
+
+
+
