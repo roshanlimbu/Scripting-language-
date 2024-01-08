34048f7e2c089a70146dba6e579ccf14c29f181a
roshanlimbu <birenlimbu415@gmail.com>
2024-01-08 07:48:54 +0545

    sort updated with numeric sort

--- labWorks/sort.js
+++ labWorks/sort.js
@@ -1,2 +1,23 @@
-const strArr = ["Nasa", "ESA", "JWST", "NAS"];
-console.log(strArr.sort());
+const strArr = ["Nasa10", "ESA11", "JWST", "NAS"];
+
+console.log("Before sorting: ", strArr);
+const numericSort = (a, b) => {
+  const numA = parseFloat(a); // converting string to float 
+  const numB = parseFloat(b); // converting string to float 
+
+
+  // if (!isNaN(numA) && !isNaN(numB)) { // checking here if there's empty values
+  //
+  //   return numA - numB;
+  // }
+
+  return a.localeCompare(b);
+
+}
+strArr.sort(numericSort);
+
+console.log("After sorting: ", strArr);
+
+
+
+
