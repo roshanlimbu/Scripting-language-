61c4fd8849bfdbd452024421ab87e64362eed0d1
roshanlimbu <birenlimbu415@gmail.com>
2024-01-18 20:58:55 +0545

    -modified index.js

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 //   heading.style.backgroundColor = "red";
 // });
 console.log("hello");
+
+
+let count = 0;
+
+for (i = 2; i <= 100; i++) {
+  for (j = 1; j <= i; j++) {
+    if (i % j === 0) {
+      count + 1;
+    }
+    if (count == 2) {
+      return j;
+    }
+  }
+}
