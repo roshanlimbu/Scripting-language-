c0943406883823c9a30f08eadd58eb228761d7f2
roshanlimbu <birenlimbu415@gmail.com>
2024-02-29 07:54:50 +0545

    -mupdated

---
+++ php/empTable.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php $emp = [
+  [1, "anup", "manager", 30000],
+  [2, "bishal", "sales man", 15000],
+  [3, "bisnu", "peon", 5000],
+];
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["email"]; ?>
+
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["email"]; ?>
+
+</body>
+</html>
