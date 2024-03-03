be9d689c46e139b5bed8a177e7f3002ded26bf58
roshanlimbu <birenlimbu415@gmail.com>
2024-03-03 20:48:03 +0545

    -updated

--- php/empTable.php
+++ php/empTable.php
@@ -5,11 +5,6 @@
   [2, "bishal", "sales man", 15000],
   [3, "bisnu", "peon", 5000],
 ];
-<?php echo $_POST["name"]; ?><br>
-<?php echo $_POST["email"]; ?>
-
-<?php echo $_POST["name"]; ?><br>
-<?php echo $_POST["email"]; ?>
 
 </body>
 </html>
